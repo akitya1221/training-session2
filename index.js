5e95520606e8894778e3d4fdb8ba421a3e5af881
akitya1221 <68094481+akitya1221@users.noreply.github.com>
2021-11-30 18:37:02 +0900

    改良版

---
+++ index.js
@@ -0,0 +1,96 @@
+// スライド設定
+$('#slider').slick({ //{}を入れる
+  autoplay: true, 
+  arrows: true,
+  dots: true,
+  dotsClass: 's-dots',
+  fade: false,
+  vertical: true,
+  verticalSwiping: true,
+  speed: 500,
+  autoplaySpeed: 1000
+});
+ 
+
+
+//  クイズ用プログラム
+let q_text1 = document.getElementById("quiz-tx1");
+q_text1.innerHTML = "次のうち腐らないものは？";
+
+let q_text2 = document.getElementById("quiz-tx2");
+q_text2.innerHTML = "カルボナーラとはどういう意味？";
+
+let q_text3 = document.getElementById("quiz-tx3");
+q_text3.innerHTML = "ラーメンの丼にある渦巻き模様は、何を意味している？";
+
+let q_text4 = document.getElementById("quiz-tx4");
+q_text4.innerHTML = "大根は白に見えるが本来異なる色である。その本来の色とは？";
+
+let q_text5 = document.getElementById("quiz-tx5");
+q_text5.innerHTML = "OCR都築の嫌いな食べ物はどれ？";
+
+
+// 正解判定
+function butotnClick() {
+
+  let score = 0;
+
+  let checkValue1 = q1.elements['answer'].value;
+  if (checkValue1 == "c") {
+    console.log("正解");
+    score++;
+  };
+
+  let checkValue2 = q2.elements['answer'].value;
+  if (checkValue2 == "b") {
+    console.log("正解");
+    score++;
+  };
+
+  let checkValue3 = q3.elements['answer'].value;
+  if (checkValue3 == "a") {
+    console.log("正解");
+    score++;
+  };
+
+  let checkValue4 = q4.elements['answer'].value;
+  if (checkValue4 == "b") {
+    console.log("正解");
+    score++;
+  };
+
+  let checkValue5 = q5.elements['answer'].value;
+  if (checkValue5 == "c") {
+    console.log("正解");
+    score++;
+  };
+
+  if (score == 5) {
+    let scoreArea = document.getElementById("score-area");
+    scoreArea.innerHTML = "5/5正解！ 満点です！";
+  } else if (score == 4) {
+    let scoreArea = document.getElementById("score-area");
+    scoreArea.innerHTML = "5/4正解！　惜しい！";
+  } else if (score == 3) {
+    let scoreArea = document.getElementById("score-area");
+    scoreArea.innerHTML = "5/3正解！　あとちょっと！";
+  } else if (score == 2) {
+    let scoreArea = document.getElementById("score-area");
+    scoreArea.innerHTML = "5/2正解！　ちょっと頑張ろう！";
+  } else if (score == 1) {
+    let scoreArea = document.getElementById("score-area");
+    scoreArea.innerHTML = "5/1正解！　がんばりましょう、、、！";
+  } else {
+    let scoreArea = document.getElementById("score-area");
+    scoreArea.innerHTML = "5/0　ぴえん、、、";
+  }
+
+};
+
+let checkButton = document.getElementById("checkButton");
+checkButton.addEventListener("click", butotnClick);
+
+function cssChange() {
+  let change = document.getElementById("change");
+  change.className = "cssAfter";
+}
